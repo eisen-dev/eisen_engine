bbec36285ff91f57f840b70e56b9c00c3c489482
Alice Ferrazzi <alice.ferrazzi@gmail.com>
2015-07-06 04:28:16 +0000

    Added portage package list method

--- server.py
+++ server.py
@@ -9,8 +9,21 @@ class RequestHandler(pyjsonrpc.HttpRequestHandler):
   @pyjsonrpc.rpcmethod
   def add(self, a, b):
       """Test method"""
-      return a + b
+      return a+b
 
+  @pyjsonrpc.rpcmethod
+  def get_all_packages(overlay_path):
+        import portage
+        from gentoolkit.package import Package
+        """Returns all packages from a given overlay path.
+        """
+        packages = {}
+        porttree = portage.db[portage.root]['porttree']
+        vartree = portage.db[portage.root]['vartree']
+        ins_pkg = []
+        for cp in porttree.dbapi.cp_all():
+            ins_pkg.append(cp)
+        return ins_pkg
 
 # Threading HTTP-Server
 http_server = pyjsonrpc.ThreadingHttpServer(
