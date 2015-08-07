3903b82b76832935830a9314fb85828c34ce259e
Alice Ferrazzi <alice.ferrazzi@gmail.com>
2015-08-07 07:33:38 +0000

    - added get installed package function

--- server.py
+++ server.py
@@ -26,6 +26,21 @@ class RequestHandler(pyjsonrpc.HttpRequestHandler):
                 ins_pkg.append(portage.catpkgsplit(i))
         return ins_pkg
 
+  @pyjsonrpc.rpcmethod
+  def get_installed_packages(overlay_path):
+        import portage
+        from gentoolkit.package import Package
+        """Returns all packages from a given overlay path.
+        """
+        packages = {}
+        porttree = portage.db[portage.root]['porttree']
+        vartree = portage.db[portage.root]['vartree']
+        ins_pkg = []
+        for cp in porttree.dbapi.cp_all():
+            for i in vartree.dbapi.cp_list(cp):
+                ins_pkg.append(portage.catpkgsplit(i))
+        return ins_pkg
+
 # Threading HTTP-Server
 http_server = pyjsonrpc.ThreadingHttpServer(
     server_address = ('', 8080),
