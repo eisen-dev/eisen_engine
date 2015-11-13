0afd2bf56095fa331634bd353d9fec35f5199cbc
alice ferrazzi <alice.ferrazzi@gmail.com>
2015-11-13 11:06:30 +0900

    Rest-API return hosts and groups

--- api.py
+++ api.py
@@ -5,7 +5,7 @@
 from flask import Flask, jsonify, abort, make_response
 from flask.ext.restful import Api, Resource, reqparse, fields, marshal
 from flask.ext.httpauth import HTTPBasicAuth
-from core import Ansible
+from core import dispatcher
 
 app = Flask(__name__, static_url_path="")
 api = Api(app)
@@ -25,13 +25,18 @@ def unauthorized():
     # auth dialog
     return make_response(jsonify({'message': 'Unauthorized access'}), 403)
 
-hosts = Ansible.HostsList()
+module = dispatcher.use_module()
+groups = dispatcher.GroupsList(module)
+hosts = dispatcher.HostsList(module)
 
 host_fields = {
+    'host': fields.String,
+    'uri': fields.Url('host')
+}
+
+group_fields = {
     'hosts': fields.String,
-    'jobs': fields.String,
     'group': fields.String,
-    'busy': fields.Boolean,
     'uri': fields.Url('host')
 }
 
@@ -43,26 +48,22 @@ class GroupsHostsAPI(Resource):
         self.reqparse.add_argument('hosts', type=str, required=True,
                                    help='No task title provided',
                                    location='json')
-        self.reqparse.add_argument('jobs', type=str, default="",
-                                   location='json')
         self.reqparse.add_argument('group', type=str, default="",
                                    location='json')
         super(GroupsHostsAPI, self).__init__()
 
     def get(self):
-        return {'hosts': [marshal(host, host_fields) for host in hosts]}
+        return {'groups': [marshal(group, group_fields) for group in groups]}
 
     def post(self):
         args = self.reqparse.parse_args()
-        host = {
+        group = {
             'id': hosts[-1]['id'] + 1,
-            'hosts': args['host'],
-            'jobs': args['jobs'],
+            'host': args['host'],
             'group': args['groups'],
-            'busy': False
         }
-        hosts.append(host)
-        return {'host': marshal(host, host_fields)}, 201
+        groups.append(group)
+        return {'host': marshal(group, group_fields)}, 201
 
 class GroupsAPI(Resource):
     decorators = [auth.login_required]
@@ -70,57 +71,51 @@ class GroupsAPI(Resource):
     def __init__(self):
         self.reqparse = reqparse.RequestParser()
         self.reqparse.add_argument('host', type=str, location='json')
-        self.reqparse.add_argument('jobs', type=str, location='json')
         self.reqparse.add_argument('groups', type=str, location='json')
-        self.reqparse.add_argument('busy', type=bool, location='json')
         super(GroupsAPI, self).__init__()
 
     def get(self, id):
-        host = [host for host in hosts if host['id'] == id]
-        if len(host) == 0:
+        group = [group for group in groups if group['id'] == id]
+        if len(group) == 0:
             abort(404)
-        return {'host': marshal(host[0], host_fields)}
+        return {'host': marshal(group[0], group_fields)}
 
     def put(self, id):
-        host = [host for host in hosts if host['id'] == id]
-        if len(host) == 0:
+        group = [group for group in groups if group['id'] == id]
+        if len(group) == 0:
             abort(404)
-        host = host[0]
+        group = group[0]
         args = self.reqparse.parse_args()
         for k, v in args.items():
             if v is not None:
-                host[k] = v
-        return {'host': marshal(host, host_fields)}
+                group[k] = v
+        return {'host': marshal(group, group_fields)}
 
     def delete(self, id):
-        host = [host for host in hosts if host['id'] == id]
-        if len(host) == 0:
+        group = [group for group in groups if group['id'] == id]
+        if len(group) == 0:
             abort(404)
-        hosts.remove(host[0])
+        groups.remove(group[0])
         return {'result': True}
 
-class TasksHostsAPI(Resource):
+class AllHostsAPI(Resource):
     decorators = [auth.login_required]
 
     def __init__(self):
         self.reqparse = reqparse.RequestParser()
-        self.reqparse.add_argument('hosts', type=str, required=True,
+        self.reqparse.add_argument('host', type=str, required=True,
                                    help='No task title provided',
                                    location='json')
-        self.reqparse.add_argument('jobs', type=str, default="",
-                                   location='json')
-        self.reqparse.add_argument('group', type=str, default="",
-                                   location='json')
-        super(TasksHostsAPI, self).__init__()
+        super(AllHostsAPI, self).__init__()
 
     def get(self):
-        return {'hosts': [marshal(host, host_fields) for host in hosts]}
+        return {'host': [marshal(host, host_fields) for host in hosts]}
 
     def post(self):
         args = self.reqparse.parse_args()
         host = {
             'id': hosts[-1]['id'] + 1,
-            'hosts': args['host'],
+            'host': args['host'],
             'jobs': args['jobs'],
             'group': args['groups'],
             'busy': False
@@ -128,16 +123,13 @@ class TasksHostsAPI(Resource):
         hosts.append(host)
         return {'host': marshal(host, host_fields)}, 201
 
-class TasksAPI(Resource):
+class HostsAPI(Resource):
     decorators = [auth.login_required]
 
     def __init__(self):
         self.reqparse = reqparse.RequestParser()
         self.reqparse.add_argument('host', type=str, location='json')
-        self.reqparse.add_argument('jobs', type=str, location='json')
-        self.reqparse.add_argument('groups', type=str, location='json')
-        self.reqparse.add_argument('busy', type=bool, location='json')
-        super(TasksAPI, self).__init__()
+        super(HostsAPI, self).__init__()
 
     def get(self, id):
         host = [host for host in hosts if host['id'] == id]
@@ -163,10 +155,10 @@ class TasksAPI(Resource):
         hosts.remove(host[0])
         return {'result': True}
 
-api.add_resource(GroupsHostsAPI, '/todo/api/v1.0/hosts', endpoint='hosts')
-api.add_resource(GroupsAPI, '/todo/api/v1.0/host/<int:id>', endpoint='host')
-api.add_resource(TasksHostsAPI, '/todo/api/v1.0/tasks', endpoint='tasks')
-api.add_resource(TasksAPI, '/todo/api/v1.0/task/<int:id>', endpoint='task')
+api.add_resource(GroupsHostsAPI, '/todo/api/v1.0/groups', endpoint='hosts')
+api.add_resource(GroupsAPI, '/todo/api/v1.0/group/<int:id>', endpoint='host')
+api.add_resource(AllHostsAPI, '/todo/api/v1.0/hosts', endpoint='tasks')
+api.add_resource(HostsAPI, '/todo/api/v1.0/host/<int:id>', endpoint='task')
 
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0')
---
+++ core/dispatcher.py
@@ -0,0 +1,34 @@
+import glob
+from genericpath import isfile
+from os.path import dirname, basename
+import Ansible
+
+def ModulesList():
+    modules = glob.glob(dirname(__file__) + "/*.py")
+
+    # remove itself for not reimporting
+    dispatcher = (glob.glob(dirname(__file__) + "/dispatcher.py"))
+    modules.remove(dispatcher[0])
+
+    __all__ = [basename(f)[:-3] for f in modules if isfile(f)]
+    #mod = {}
+    #for i in __all__:
+    #    print i
+    #    mod[i]=(__import__('core.'+ i))
+    return __all__
+
+# return usable mod
+#def installed_mod():
+    #return(mod)
+
+def use_module():
+    module = Ansible;
+    return module
+
+def HostsList(module):
+    hosts = module.HostsList()
+    return hosts
+
+def GroupsList(module):
+    groups = module.GroupsList()
+    return groups
