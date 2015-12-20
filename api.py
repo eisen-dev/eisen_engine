fe602e7f747319a4a3ca5ce0d10bee136c9d0b29
alice ferrazzi <alice.ferrazzi@gmail.com>
2015-12-21 01:12:18 +0900

    Changed api url from /todo/api/ to /eisen/api/

--- api.py
+++ api.py
@@ -42,18 +42,18 @@ def unauthorized():
     # auth dialog
     return make_response(jsonify({'message': 'Unauthorized access'}), 403)
 
-api.add_resource(AgentInfo.AgentAPI, '/todo/api/v1.0/agent', endpoint='agent')
-api.add_resource(GroupsList.GroupsAPI, '/todo/api/v1.0/groups', endpoint='groups')
-api.add_resource(GroupsList.GroupAPI, '/todo/api/v1.0/group/<int:id>', endpoint='group')
-api.add_resource(GroupsList.GroupVarsAPI, '/todo/api/v1.0/group/<int:id>/vars',
+api.add_resource(AgentInfo.AgentAPI, '/eisen/api/v1.0/agent', endpoint='agent')
+api.add_resource(GroupsList.GroupsAPI, '/eisen/api/v1.0/groups', endpoint='groups')
+api.add_resource(GroupsList.GroupAPI, '/eisen/api/v1.0/group/<int:id>', endpoint='group')
+api.add_resource(GroupsList.GroupVarsAPI, '/eisen/api/v1.0/group/<int:id>/vars',
                  endpoint='groupvars')
-api.add_resource(HostsList.HostsAPI, '/todo/api/v1.0/hosts', endpoint='hosts')
-api.add_resource(HostsList.HostAPI, '/todo/api/v1.0/host/<int:id>', endpoint='host')
-api.add_resource(HostsList.HostVarsAPI, '/todo/api/v1.0/host/<int:id>/vars',
+api.add_resource(HostsList.HostsAPI, '/eisen/api/v1.0/hosts', endpoint='hosts')
+api.add_resource(HostsList.HostAPI, '/eisen/api/v1.0/host/<int:id>', endpoint='host')
+api.add_resource(HostsList.HostVarsAPI, '/eisen/api/v1.0/host/<int:id>/vars',
                  endpoint='hostvars')
-api.add_resource(Tasks.TasksAPI, '/todo/api/v1.0/tasks', endpoint='tasks')
-api.add_resource(Tasks.TaskAPI, '/todo/api/v1.0/task/<int:id>', endpoint='task')
-api.add_resource(Tasks.TaskRunAPI, '/todo/api/v1.0/task/<int:id>/run',
+api.add_resource(Tasks.TasksAPI, '/eisen/api/v1.0/tasks', endpoint='tasks')
+api.add_resource(Tasks.TaskAPI, '/eisen/api/v1.0/task/<int:id>', endpoint='task')
+api.add_resource(Tasks.TaskRunAPI, '/eisen/api/v1.0/task/<int:id>/run',
                  endpoint='taskrun')
 
 if __name__ == '__main__':
