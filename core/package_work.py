7e25e708b7f75f7532e10ecaab69dcb841b74d41
alice ferrazzi <alice.ferrazzi@gmail.com>
2016-02-02 22:59:23 +0900

    Returning the result for be sended from the rest api

--- core/package_work.py
+++ core/package_work.py
@@ -1,11 +1,16 @@
-from os_dependant import os_install_command
+from os_dependant import os_install_command, os_remove_command, os_update_command
 
 def package_get(pack):
     if pack['packageAction'] == 'install':
         print 'install package'
-        os_install_command(pack)
+        result_string = os_install_command(pack)
+        return result_string
     elif pack['packageAction'] == 'update':
         print 'update package'
+        result_string = os_update_command(pack)
+        return result_string
     elif pack['packageAction'] == 'delete':
         print 'deleting package'
+        result_string = os_remove_command(pack)
+        return result_string
 
